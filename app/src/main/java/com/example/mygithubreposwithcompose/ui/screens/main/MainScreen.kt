2581d2ab069c2f127614c701d3a09028ef999a82
joNA <jitzian@gmail.com>
2022-04-23 15:13:59 -0400

    reposWithCompose jc - Wiring up the MainScreen displaying. Cleanup is required

--- app/src/main/java/com/example/mygithubreposwithcompose/ui/screens/main/MainScreen.kt
+++ app/src/main/java/com/example/mygithubreposwithcompose/ui/screens/main/MainScreen.kt
@@ -2,13 +2,17 @@ package com.example.mygithubreposwithcompose.ui.screens.main
 
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.annotation.ExperimentalCoilApi
 import com.example.mygithubreposwithcompose.rest.model.ResultApiItem
+import com.example.mygithubreposwithcompose.ui.app.ReposApp
 import com.example.mygithubreposwithcompose.ui.common.ItemRow
+import com.example.mygithubreposwithcompose.ui.common.MainAppBar
+import com.example.mygithubreposwithcompose.ui.screens.error.ConnectivityError
 
 /**
  * Note for Reference: https://dev.to/paulallies/jetpack-compose-api-data-to-list-view-5fki
@@ -17,23 +21,48 @@ import com.example.mygithubreposwithcompose.ui.common.ItemRow
 
 @ExperimentalCoilApi
 @Composable
-fun MainScreenState(user: String, mainViewModel: MainViewModel = viewModel()) {
+fun MainScreenState(
+    mainViewModel: MainViewModel = viewModel(),
+    onRepoClick: (Int) -> Unit
+) {
 
     val data by mainViewModel.data.collectAsState()
-    mainViewModel.getRepos(user = user)
+    mainViewModel.getRepos("jitzian")
 
-    if (!data.isError) {
-        data.repos?.let { MainScreen(it) }
+    if (data.isError) {
+        ConnectivityError()
+    } else {
+        data.repos?.let {
+            MainScreen(
+                repos = it,
+                onRepoClick = onRepoClick
+            )
+        }
     }
 }
 
 
 @ExperimentalCoilApi
 @Composable
-fun MainScreen(repos: List<ResultApiItem>) {
-    LazyColumn {
-        items(repos) { item ->
-            ItemRow(data = item)
+fun MainScreen(repos: List<ResultApiItem>, onRepoClick: (Int) -> Unit) {
+    ReposApp {
+        Scaffold(
+            topBar = {
+                MainAppBar()
+            }
+        ) {
+            LazyColumn {
+                items(repos) { item ->
+                    item.id?.let { safeRepoId ->
+                        ItemRow(
+                            data = item,
+                            onRepoClick = {
+                                onRepoClick(safeRepoId)
+                            }
+                        )
+                    }
+                }
+            }
         }
     }
 }
