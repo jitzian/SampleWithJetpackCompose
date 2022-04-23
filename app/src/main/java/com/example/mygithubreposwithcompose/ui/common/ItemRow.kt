e8fffb3aa0788557fbad0683f60e2114e2e439bb
joNA <jitzian@gmail.com>
2022-04-23 14:53:38 -0400

    reposWithCompose jc - Update dimensions

--- app/src/main/java/com/example/mygithubreposwithcompose/ui/common/ItemRow.kt
+++ app/src/main/java/com/example/mygithubreposwithcompose/ui/common/ItemRow.kt
@@ -1,26 +1,31 @@
 package com.example.mygithubreposwithcompose.ui.common
 
+import android.util.Log
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.res.dimensionResource
 import coil.annotation.ExperimentalCoilApi
+import com.example.mygithubreposwithcompose.R
 import com.example.mygithubreposwithcompose.rest.model.ResultApiItem
 import java.util.*
 
 @ExperimentalCoilApi
 @Composable
-fun ItemRow(data: ResultApiItem) {
+fun ItemRow(data: ResultApiItem, onRepoClick: (Int) -> Unit) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(8.dp),
-        elevation = 4.dp
+            .padding(dimensionResource(id = R.dimen.dimen_8_dp))
+            .clickable {
+                Log.e("ItemRow", "ItemRow: ")
+                onRepoClick.invoke(data.id!!)
+            },
+        elevation = dimensionResource(id = R.dimen.dimen_4_dp)
     ) {
         Row {
             Thumb(data = data)
@@ -33,14 +38,14 @@ fun ItemRow(data: ResultApiItem) {
                     text = "${data.name?.uppercase(Locale.ROOT)}",
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(8.dp),
+                        .padding(dimensionResource(id = R.dimen.dimen_8_dp)),
                     style = MaterialTheme.typography.subtitle2,
                 )
                 Text(
                     text = "${data.description}",
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(8.dp),
+                        .padding(dimensionResource(id = R.dimen.dimen_8_dp)),
                     maxLines = 2,
                 )
             }
