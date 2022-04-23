cc18035bb4602c41ad0fcc6184fc711ec44e6a41
joNA <jitzian@gmail.com>
2022-04-23 14:41:52 -0400

    reposWithCompose jc - Interation of App bar in the details screen. Still it is a dummy page but I'll continue working on it

--- app/src/main/java/com/example/mygithubreposwithcompose/ui/screens/detail/DetailScreen.kt
+++ app/src/main/java/com/example/mygithubreposwithcompose/ui/screens/detail/DetailScreen.kt
@@ -5,30 +5,37 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.mygithubreposwithcompose.R
+import com.example.mygithubreposwithcompose.ui.app.ReposApp
+import com.example.mygithubreposwithcompose.ui.common.MainAppBar
 
 @Composable
 fun DetailScreen(onUpClick: () -> Unit) {
+    ReposApp {
+        Scaffold(
+            topBar = { MainAppBar() }
+        ) {
+            Box(modifier = Modifier.padding(16.dp)) {
+                Column {
+                    Text(
+                        modifier = Modifier.fillMaxWidth(),
+                        text = "Title", style = MaterialTheme.typography.subtitle1
+                    )
 
-    Box(modifier = Modifier.padding(16.dp)) {
-        Column {
-            Text(
-                modifier = Modifier.fillMaxWidth(),
-                text = "Title", style = MaterialTheme.typography.subtitle1
-            )
-
-            Text(
-                text = stringResource(id = R.string.loremIpsum),
-                modifier = Modifier.padding(
-                    top = 8.dp
-                )
-            )
+                    Text(
+                        text = stringResource(id = R.string.loremIpsum),
+                        modifier = Modifier.padding(
+                            top = 8.dp
+                        )
+                    )
+                }
+            }
         }
     }
 
