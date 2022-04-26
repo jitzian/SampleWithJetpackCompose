f79d23c245f5da6b0455f977d5a0d76ee642dbad
joNA <jitzian@gmail.com>
2022-04-26 13:42:47 -0400

    ReposWithCompose jc - Ummm.. I don't remember what I did here... it was late night...

--- app/src/main/java/com/example/mygithubreposwithcompose/rest/RestApi.kt
+++ app/src/main/java/com/example/mygithubreposwithcompose/rest/RestApi.kt
@@ -1,10 +1,17 @@
 package com.example.mygithubreposwithcompose.rest
 
 import com.example.mygithubreposwithcompose.rest.model.ResultApi
+import com.example.mygithubreposwithcompose.rest.model.ResultApiItem
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface RestApi {
     @GET("users/{user}/repos")
     suspend fun getRepos(@Path("user") user: String): ResultApi
+
+    @GET("repos/{user}/{id}")
+    suspend fun getRepoDetailsById(
+        @Path("user") user: String,
+        @Path("id") id: String
+    ): ResultApiItem?
 }
