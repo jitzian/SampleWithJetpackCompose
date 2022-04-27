3efe7d9c1650f98a3399a38ccdf5fe4c1034b651
joNA <jitzian@gmail.com>
2022-04-27 18:24:06 -0400

    ReposWithCompose jc - First Iteration of BackButton. Still pending the implementation of the action (back button)

---
+++ app/src/main/java/com/example/mygithubreposwithcompose/ui/common/ArrowBackIcon.kt
@@ -0,0 +1,21 @@
+package com.example.mygithubreposwithcompose.ui.common
+
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.example.mygithubreposwithcompose.R
+
+@Composable
+fun ArrowBackIcon(showBackButton: Boolean = false) {
+    if (showBackButton) {
+        IconButton(onClick = { /*onClick()*/ }) {
+            Icon(
+                imageVector = Icons.Default.ArrowBack,
+                contentDescription = stringResource(id = R.string.go_to_previous_screen)
+            )
+        }
+    }
+}
