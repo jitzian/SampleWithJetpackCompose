98a9114bd0dc49d04857c4c15d1bf771ed393930
joNA <jitzian@gmail.com>
2022-04-28 20:28:16 -0400

    ReposWithCompose jc - Adding dismissal action when clicked on element in dropdown menu

--- app/src/main/java/com/example/mygithubreposwithcompose/ui/common/MainAppBar.kt
+++ app/src/main/java/com/example/mygithubreposwithcompose/ui/common/MainAppBar.kt
@@ -4,6 +4,10 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.res.stringResource
 import com.example.mygithubreposwithcompose.R
 
@@ -17,7 +21,9 @@ fun MainAppBar(
 ) {
 
     val listOfMenu = listOf("Option 1", "Option 2", "Option 3")
-
+    var showMenu by rememberSaveable {
+        mutableStateOf(false)
+    }
 
     if (showBackButton) {
         TopAppBar(
@@ -29,16 +35,18 @@ fun MainAppBar(
             },
             actions = {
                 if (showMore) {
-                    IconButton(onClick = { /*TODO*/ }) {
+                    IconButton(onClick = {
+                        showMenu = !showMenu
+                    }) {
                         Icon(
                             imageVector = Icons.Default.MoreVert,
                             contentDescription = stringResource(
                                 id = R.string.more_options_text
                             )
                         )
-                        DropdownMenu(expanded = true, onDismissRequest = { /*TODO*/ }) {
+                        DropdownMenu(expanded = showMenu, onDismissRequest = { showMenu = false }) {
                             listOfMenu.forEach { option ->
-                                DropdownMenuItem(onClick = { /*TODO*/ }) {
+                                DropdownMenuItem(onClick = { showMenu = false }) {
                                     ListItem(text = { Text(text = option) })
                                 }
                             }
